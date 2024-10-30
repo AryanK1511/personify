4a2137ca4a1dcf0241d3fbbd68872f674851fa4a
Aryan Khurana <aryankhurana1511@gmail.com>
2024-10-30 15:27:37 -0400

    chore: clean up code and save changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -13,8 +13,4 @@ CORS(app, supports_credentials=True)
 app.config.from_object(Config)
 
 # Import the routes
-<<<<<<< HEAD
 from app.routes import get
-=======
-from app.routes import post
->>>>>>> main
--- backend/app/config.py
+++ backend/app/config.py
@@ -2,19 +2,5 @@ import os
 
 
 class Config:
-<<<<<<< HEAD
     # ===== GOOGLE GEMINI CONFIG =====
     GOOGLE_GEMINI_API_KEY = os.environ.get('GOOGLE_GEMINI_API_KEY') or None
-=======
-    pass
-    # # ===== POSTGRESQL CONFIG =====
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or None
-
-    # # ===== CLOUDINARY CONFIG =====
-    # CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
-    # CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
-    # CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
-
-    # # ==== JWT SECRET ====
-    # JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
->>>>>>> main
